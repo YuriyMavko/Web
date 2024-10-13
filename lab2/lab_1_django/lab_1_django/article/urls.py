f35defeea1b7292e3c94ab6e9fbf475d198e6237
YuriyMavko <ymavko@gmail.com>
2024-10-13 12:08:27 +0300

    .

--- lab2/lab_1_django/lab_1_django/article/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index, create, delete_article
-
-urlpatterns = [
-    path('', index, name='index'),
-    path('create/', create, name='create'),
-    path('delete/<int:id>/', delete_article, name='delete_article'),
-]
--- lab2/lab_1_django/lab_1_django/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.page_one, name='home'),
-    path('page_one/', views.page_one, name='page_one'),
-    path('page_two/', views.page_two, name='page_two'),
-]
